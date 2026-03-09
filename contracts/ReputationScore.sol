7678a76e4150b900c79f06cc833cd95cdfb0d413
badboyzino <boyx100vip@gmail.com>
2026-03-09 22:25:07 +0700

    Update ReputationScore.sol

--- contracts/ReputationScore.sol
+++ contracts/ReputationScore.sol
@@ -1,1 +1,10 @@
+pragma solidity ^0.8.0;
 
+contract ReputationScore {
+
+    mapping(address => uint) public score;
+
+    function updateScore(address user, uint value) public {
+        score[user] += value;
+    }
+}
