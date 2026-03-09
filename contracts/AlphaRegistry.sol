8ad0ac17b6e48f86cac60132af064b5c3814eeb7
badboyzino <boyx100vip@gmail.com>
2026-03-09 22:24:43 +0700

    Update AlphaRegistry.sol

--- contracts/AlphaRegistry.sol
+++ contracts/AlphaRegistry.sol
@@ -1,1 +1,17 @@
+pragma solidity ^0.8.0;
 
+contract AlphaRegistry {
+
+    struct AlphaCall {
+        string project;
+        string thesis;
+        uint timestamp;
+        address researcher;
+    }
+
+    AlphaCall[] public calls;
+
+    function submitAlpha(string memory project, string memory thesis) public {
+        calls.push(AlphaCall(project, thesis, block.timestamp, msg.sender));
+    }
+}
